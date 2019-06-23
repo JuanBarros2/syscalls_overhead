810f0a551eba7b111b0c92c61ad99b3273924fe5
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2019-06-23 17:49:52 -0300

    Gera dados para perf

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,10 @@ COPY ./codes/pesquisaOverhead.c /pesquisaOverhead.c
 
 RUN apt-get update &&\
     apt-get install -y build-essential &&\
+    apt-get install -y --no-install-recommends linux-perf && \
+    ln -s /usr/bin/perf_* /usr/bin/perf && \
     apt-get install -y strace &&\
     apt-get clean &&\
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* &&\
     gcc -o /memoriaOverhead /memoriaOverhead.c &&\
     gcc -o pesquisaOverhead /pesquisaOverhead.c 
--- run_all.sh
+++ run_all.sh
@@ -1,24 +1,31 @@
 #/bin/bash
-docker build -t projso/strace:1.0 .
+docker build -t projso/docker:1.0 .
 
 if [ ! -d "./reports" ] 
 then
     mkdir reports
-    mkdir ./reports/hip1
-    mkdir ./reports/hip4
+    for hip in hip1 hip4
+    do
+        mkdir ./reports/$hip
+        mkdir ./reports/$hip/perf
+        mkdir ./reports/$hip/strace
+    done
 fi
 
 ## Hipotese 1 - Memória influencia em syscall
 for syscall in fork execvp clone posix_spawnp
 do
-    if [ ! -d "./reports/hip1/$syscall" ] 
-    then
-        mkdir ./reports/hip1/$syscall
-    fi
-
+    for tool in strace perf
+    do
+        if [ ! -d "./reports/hip1/$tool/$syscall" ] 
+        then
+            mkdir ./reports/hip1/$tool/$syscall
+        fi
+    done
     for memoryLevel in 10 100
     do
-        docker run -it projso/strace:1.0 strace -c ./memoriaOverhead $syscall $memoryLevel > ./reports/hip1/$syscall/$memoryLevel.txt
+        docker run -it projso/docker:1.0 strace -c ./memoriaOverhead $syscall $memoryLevel > ./reports/hip1/strace/$syscall/$memoryLevel.txt
+        docker run -it --privileged projso/docker:1.0 perf stat ./memoriaOverhead $syscall $memoryLevel > ./reports/hip1/perf/$syscall/$memoryLevel.txt
     done
 done
 
@@ -26,6 +33,7 @@ done
 ## Hipotese 4 - Memória influencia em syscall
 for syscall in execv execvp posix_spawn posix_spawnp
 do
-    docker run -it projso/strace:1.0 strace -c ./pesquisaOverhead $syscall  > ./reports/hip4/$syscall.txt
+    docker run -it projso/docker:1.0 strace -c ./pesquisaOverhead $syscall  > ./reports/hip4/strace/$syscall.txt
+    docker run --privileged -it projso/docker:1.0 perf stat ./pesquisaOverhead $syscall  > ./reports/hip4/perf/$syscall.txt
 done
 
