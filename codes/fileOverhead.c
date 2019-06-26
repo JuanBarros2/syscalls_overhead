6b8f61e677ddcd75b061e3fbd8ec973c56deb84b
Diego Takei <diegotakei1586@gmail.com>
2019-06-26 09:19:59 -0300

    add fileOverhead code

---
+++ codes/fileOverhead.c
@@ -0,0 +1,65 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sched.h>
+#include <spawn.h>
+
+#define FILE_NAME "files/text"
+
+
+int doNothing() {
+    return 0;
+}
+
+void createFiles(int files, char syscall[]) {
+    pid_t pid;
+
+    char* argv[] = { "sleep" , "0", NULL};
+
+    for(int i = 0; i<files; i++){
+        char x [100] = FILE_NAME;
+        // strcat(x, itoa(i));
+        // strcat(x, ".txt");
+        sprintf(x, "%s%d.txt", FILE_NAME, i);
+        FILE* file_ptr = fopen(x, "w");
+        fclose(file_ptr);
+    }
+
+    if (strcmp(syscall, "fork") == 0){
+        /* Cria uma cópia do processo atual e continua a execução
+        dos dois processos.*/
+        fork();
+    } else if(strcmp(syscall, "execvp") == 0) {
+        /* Substitui a imagem do processo atual por um novo
+        processo. Recebe o nome da imagem a ser pesquisada e um
+        array de argumentos a ser passado. */
+        int erro = execvp(argv[0], argv);
+    } else if(strcmp(syscall, "clone") == 0) {
+        /* Cria uma thread de execução para o processo atual.
+        Recebe um ponteiro de uma função que rodará na execução
+        da syscall, um ponteiro para a pilha compartilhada entre
+        threads, constantes (nesse caso, CLONE_VFORK define que
+        o processo pai deve esperar os filhos terminarem a execução
+        para continuar a execução), e ponteiro para argumentos.*/
+        clone(&doNothing, NULL, CLONE_VFORK, NULL);
+    } else if(strcmp(syscall, "posix_spawnp") == 0) {
+        pid_t pid;
+        posix_spawnp(&pid, argv[0], NULL, NULL, argv, NULL);
+        exit(0);
+    }
+    
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Numero de argumentos incorreto");
+    } else {
+        createFiles(atoi(argv[2]), argv[1]);
+    }
+
+   return 0;
+}
