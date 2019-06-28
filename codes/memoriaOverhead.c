9b394066ad7658f4ff0838ef7cfe62e1bad897a9
José Renan <joserenansl99@gmail.com>
2019-06-28 11:30:16 -0300

    Corrige chamada ao clone

--- codes/memoriaOverhead.c
+++ codes/memoriaOverhead.c
@@ -12,29 +12,33 @@ int doNothing() {
 }
 
 void expandMemory(int depth, char syscall[]) {
-    pid_t pid;
     long auxMemory[depth][depth];
     if (depth == 0) {
         char* argv[] = { "sleep" , "0", NULL};
         if (strcmp(syscall, "fork") == 0){
             /* Cria uma cópia do processo atual e continua a execução
             dos dois processos.*/
-            fork();
+	    fork();
         } else if(strcmp(syscall, "execvp") == 0) {
             /* Substitui a imagem do processo atual por um novo
             processo. Recebe o nome da imagem a ser pesquisada e um
             array de argumentos a ser passado. */
             int erro = execvp(argv[0], argv);
         } else if(strcmp(syscall, "clone") == 0) {
-            /* Cria uma thread de execução para o processo atual.
+            void *pchild_stack = malloc(1024 * 1024);
+   	    if ( pchild_stack == NULL ) {
+      		printf("ERROR: Unable to allocate memory.\n");
+      		exit(EXIT_FAILURE);
+   	    }
+	    /* Cria uma thread de execução para o processo atual.
             Recebe um ponteiro de uma função que rodará na execução
             da syscall, um ponteiro para a pilha compartilhada entre
             threads, constantes (nesse caso, CLONE_VFORK define que
             o processo pai deve esperar os filhos terminarem a execução
             para continuar a execução), e ponteiro para argumentos.*/
-            clone(&doNothing, NULL, CLONE_VFORK, NULL);
+            clone(&doNothing, pchild_stack + (1024 * 1024), CLONE_VFORK, NULL);
         } else if(strcmp(syscall, "posix_spawnp") == 0) {
-            pid_t pid;
+            pid_t pid; 
             posix_spawnp(&pid, argv[0], NULL, NULL, argv, NULL);
             exit(0);
         }
